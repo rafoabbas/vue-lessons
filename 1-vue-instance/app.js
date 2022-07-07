a3f7bf52ef38cf8c5dc9ecb9ac32f00832d8b8eb
Rauf Abbas <rafo.abbas@gmail.com>
2022-07-07 16:07:05 +0400

    method and event

--- 1-vue-instance/app.js
+++ 1-vue-instance/app.js
@@ -1,1 +1,24 @@
-// const app = Vue.new
+const app = Vue.createApp({
+    data() {
+        return {
+            title : "Hello Vue.js",
+            content : "This is a content",
+            coordinates : {
+                x : 0,
+                y : 0
+            }
+        }
+    },
+    methods: {
+        changeTitle(title){
+            this.title = title;
+        },
+        updateCoordinates(event, box){
+            this.coordinates = {
+                x : event.x,
+                y : event.y
+            }
+        }
+    }
+}).mount('#app')
+
