ff2a36cde4a8a2954e4ea7423a77a1d17fa97202
Rauf Abbas <rafo.abbas@gmail.com>
2022-09-21 10:22:38 +0400

    3-counter-app

--- 3-counter-app/app.js
+++ 3-counter-app/app.js
@@ -1,11 +1,32 @@
 const app = Vue.createApp({
     data() {
         return {
-            count: 0,
+            count  : 0,
+            count2 : 0,
         }
     },
     methods: {
 
+        // inc() {
+        //     this.count ++;
+        // },
+        // dec() {
+        //     this.count --;
+        // },
+    },
+
+    //computed icinde calisan nesneler degisken gibi calisan method kimi calisan neslelerdir
+    // getCountResult2, getCountResult funksiyalarin methods icinde yazsaydiq count ve count2 her
+    // deyisdiyinde iki methodun ikiside calisacaqdi
+    computed: {
+        getCountResult2() {
+            console.log("getCountResult2");
+            return this.count2 > 5 ? 'Boyuk' : 'Kicik';
+        },
+        getCountResult() {
+            console.log("getCountResult");
+            return this.count > 5 ? 'Boyuk' : 'Kicik';
+        },
     }
 }).mount('#app')
 
